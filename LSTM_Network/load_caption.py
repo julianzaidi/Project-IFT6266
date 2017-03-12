96503262c41e42e7b5d093ec8c0d88eab97b9dfc
julianzaidi <julian.zaidi1@gmail.com>
2017-03-12 15:19:31 -0400

    LSTM model

---
+++ LSTM_Network/load_caption.py
@@ -0,0 +1,203 @@
+import os
+import glob
+import theano
+import numpy as np
+import cPickle as pkl
+import PIL.Image as Image
+from utils import get_path
+
+
+def load_caption(train="train_caption.pkl", valid="valid_caption.pkl"):
+    path = get_path()
+
+    caption_path = train
+    train_path = os.path.join(path, caption_path)
+
+    with open(train_path) as fd:
+        train_caption = pkl.load(fd)  # Keys from 0 to 82610
+
+    caption_path = valid
+    valid_path = os.path.join(path, caption_path)
+
+    with open(valid_path) as fd:
+        valid_caption = pkl.load(fd)  # Keys from 0 to 40437
+
+    return train_caption, valid_caption
+
+
+def ordered_list(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
+
+def reshape(sentence):
+    sentence = sentence.lower().replace('.', '').replace(',', '').replace(';', '').replace(':', '').replace('"', ''). \
+        replace('...', '').replace('/', '').replace('\'', '')
+    return sentence
+
+
+def get_caption():
+    train_caption, valid_caption = load_caption()
+
+    vocabulary = []
+    # train_vocabulary = []
+    # valid_vocabulary = []
+    nb_train_images = len(train_caption)
+    nb_valid_images = len(valid_caption)
+
+    for i in range(nb_train_images):
+        nb_caption = len(train_caption[i])
+        for j in range(nb_caption):
+            train_caption[i][j] = reshape(train_caption[i][j])
+            split = train_caption[i][j].split()
+            nb_words = len(split)
+            for k in range(nb_words):
+                # train_vocabulary.append(split[k])
+                vocabulary.append(split[k])
+
+    # train_vocabulary = list(set(train_vocabulary))
+    # train_vocabulary = ordered_list(train_vocabulary)
+
+    for i in range(nb_valid_images):
+        nb_caption = len(valid_caption[i])
+        for j in range(nb_caption):
+            valid_caption[i][j] = reshape(valid_caption[i][j])
+            split = valid_caption[i][j].split()
+            nb_words = len(split)
+            for k in range(nb_words):
+                # valid_vocabulary.append(split[k])
+                vocabulary.append(split[k])
+
+    # valid_vocabulary = list(set(valid_vocabulary))
+    # valid_vocabulary = ordered_list(valid_vocabulary)
+    vocabulary = ordered_list(vocabulary)
+
+    return train_caption, valid_caption, vocabulary
+
+
+# train_caption = load_caption()
+# print len(train_caption)  # 82611 = nbr images
+# print train_caption[0]  # Captions for the first image
+# print len(train_caption[0])  # Number of captions for the first image
+# print train_caption[0][0]  # First caption of the first image
+# print train_caption[0][0].split()  # List of words for the first caption of the first image
+# print len(train_caption[0][0].split())  # Number of words for the first caption of the image
+# print train_caption[0][0].split()[0] # First word of the first caption of the first image
+
+# train_caption, valid_caption, vocabulary = get_caption()
+# print train_caption[0]
+# print train_caption[6]
+# print valid_caption[0]
+# print len(train_vocabulary)
+# print len(valid_vocabulary)
+# print len(vocabulary)
+# for i in range(10):
+#    print vocabulary[i]
+# for i in range(10):
+# for i in range(10):
+#    print train_vocabulary[i]
+# for i in range(10):
+#    print valid_vocabulary[i]
+# print (train_vocabulary.index('food'))
+
+# train_caption, valid_caption = load_caption()
+# print train_caption[0][0]
+# train_caption[0][0] = reshape(train_caption[0][0])
+# print train_caption[0][0]
+
+# sentence1 = 'BonJour JULIAN , ZA " IDI'
+# print sentence1
+# print len(sentence1.split())
+# sentence1 = reshape(sentence1)
+# print sentence1
+# print len(sentence1.split())
+
+
+def get_length(caption_dict, idx, nb_caption, batch_size):
+    '''
+            Find the shortest sentence in the batch
+            :param nb_caption: the number of caption used per image
+            '''
+
+    min_length = np.inf
+    nbr_caption = nb_caption
+
+    for i in range(idx * batch_size, (idx + 1) * batch_size):
+        if nb_caption == 'max':
+            nbr_caption = len(caption_dict[i])
+        for j in range(nbr_caption):
+            length = len(caption_dict[i][j].split())
+            if length < min_length:
+                min_length = length
+
+    return min_length
+
+
+def get_number(caption_dict, idx, nb_caption, batch_size):
+    '''
+                Find the number of total caption in the batch
+                :param nb_caption: the number of caption used per image
+                '''
+
+    tot_caption = 0
+
+    if nb_caption == 'max':
+        for i in range(idx * batch_size, (idx + 1) * batch_size):
+            tot_caption += len(caption_dict[i])
+    else:
+        tot_caption = batch_size * nb_caption
+
+    return tot_caption
+
+
+def get_batch(caption_dict, vocabulary_list, idx, nb_caption, batch_size):
+    '''
+                    Compute the mini-batch
+                    :param nb_caption: the number of caption used per image
+                    '''
+
+    length = get_length(caption_dict, idx, nb_caption, batch_size)
+    tot_caption = get_number(caption_dict, idx, nb_caption, batch_size)
+    batch = np.zeros((tot_caption, length))
+    nbr_caption = nb_caption
+    iterator = 0
+
+    for i in range(idx * batch_size, (idx + 1) * batch_size):
+        if nb_caption == 'max':
+            nbr_caption = len(caption_dict[i])
+        for j in range(nbr_caption):
+            caption = [caption_dict[i][j].split()[k] for k in range(length)]
+            index = []
+            for l in range(length):
+                index.append(vocabulary_list.index(caption[l]))
+            batch[iterator, :] = index
+            iterator += 1
+
+
+def get_batches(batch_size=200, nb_caption=1):
+    '''
+            Create matrix mini-batches for the input of the network
+            :param batch_size: Number of images
+            :param nb_caption: the number of caption used per image
+            '''
+
+    train_caption, valid_caption, vocabulary = get_caption()
+
+    nb_train_images = len(train_caption)
+    nb_valid_images = len(valid_caption)
+    train_mini_batches = []
+    valid_mini_batches = []
+
+    n_train_batches = nb_train_images // batch_size  # 413 mini-batch of 200 images
+    n_valid_batches = nb_valid_images // batch_size  # 202 mini-batch of 200 images
+
+    for i in range(n_train_batches):
+        mini_batch = get_batch(train_caption, vocabulary, i, nb_caption, batch_size)
+        train_mini_batches.append(mini_batch)
+
+    for j in range(n_valid_batches):
+        mini_batch = get_batch(valid_caption, vocabulary, i, nb_caption, batch_size)
+        valid_mini_batches.append(mini_batch)
+
+    return train_mini_batches, valid_mini_batches
---
+++ LSTM_Network/model.py
@@ -0,0 +1,145 @@
+"""
+        Implementation of a LSTM network to learn the caption
+        """
+
+import theano.tensor as T
+import theano
+import numpy as np
+
+import lasagne
+import lasagne.layers as layers
+from lasagne.nonlinearities import rectify
+
+
+#########################
+# Layers implementation #
+#########################
+
+def initialize_parameters():
+    W = lasagne.init.Normal()
+    b = lasagne.init.Constant(0.)
+
+    return [W, b]
+
+
+class InputLayer(object):
+    def __init__(self, shape, input_var=None):
+        """
+                Input of the Network
+
+                """
+
+        self.output = layers.InputLayer(shape, input_var=input_var)
+
+
+class DenseLayer(object):
+    def __init__(self, input, num_units, activation=rectify):
+        """
+                Typical hidden layer of a MLP: units are fully-connected
+
+                NOTE : The non linearity used here is relu
+
+                """
+
+        self.input = input
+        self.output = layers.DenseLayer(self.input, num_units, W=initialize_parameters()[0],
+                                        b=initialize_parameters()[1],
+                                        nonlinearity=activation)
+
+
+class UnpoolLayer(object):
+    def __init__(self, input, scale=(2, 2)):
+        """
+                Allocate an UnpoolLayer
+
+                """
+
+        self.input = input
+        self.output = layers.Upscale2DLayer(self.input, scale)
+
+
+class TransposedConvLayer(object):
+    def __init__(self, input, num_filters, filter_size, stride=(2, 2), padding=(0, 0), activation=rectify):
+        """
+                Allocate a TransposedConvLayer with shared variable internal parameters.
+
+                """
+
+        self.input = input
+        self.output = layers.TransposedConv2DLayer(self.input, num_filters, filter_size, stride=stride, crop=padding,
+                                                   W=initialize_parameters()[0], b=initialize_parameters()[1],
+                                                   nonlinearity=activation)
+
+
+class EmbeddingLayer(object):
+    def __init__(self, input, input_size, output_size):
+        """
+                Allocate an Embedding Layer.
+
+                """
+
+        self.input = input
+        self.output = layers.EmbeddingLayer(self.input, input_size, output_size, W=initialize_parameters()[0])
+
+
+class LSTMLayer(object):
+    def __init__(self, input, n_hidden=500, grad_clip=100., only_return_final=True):
+
+        self.input = input
+
+        gate_parameters = layers.Gate(W_in=lasagne.init.Orthogonal(), W_hid=lasagne.init.Orthogonal(),
+                                      b=initialize_parameters()[1])
+
+        cell_parameters = layers.Gate(W_in=lasagne.init.Orthogonal(), W_hid=lasagne.init.Orthogonal(),
+                                      W_cell=None, b=initialize_parameters()[1],
+                                      nonlinearity=lasagne.nonlinearities.tanh)
+
+        self.output = layers.LSTMLayer(self.input, n_hidden, ingate=gate_parameters, forgetgate=gate_parameters,
+                                       cell=cell_parameters, outgate=gate_parameters, grad_clipping=grad_clip,
+                                       only_return_final=only_return_final)
+
+
+
+def build_model(input_var=None, input_channels=3, nfilters=[60, 30, 10, 3],
+                filter_size=[3, 3, 2, 4]):
+    ###############################
+    # Build Network Configuration #
+    ###############################
+
+    print '... Building the model'
+
+    # Input of the network : shape = (batch_size, 3, 64, 64)
+    input_layer = InputLayer(shape=(None, input_channels, 64, 64), input_var=input_var)
+
+    embedded_layer = EmbeddingLayer()
+
+    lstm_layer = LSTMLayer()
+
+    # Dense Layer : output.shape = (batch_size, 350)
+    dense_layer1 = DenseLayer(layers.FlattenLayer(pool_layer3.output), num_units=350)
+
+    # Dense Layer : output.shape = (batch_size, 350)
+    dense_layer2 = DenseLayer(dense_layer1.output, num_units=350)
+
+    # Reshape layer : output.shape = (batch_size, 350, 1, 1)
+    reshape_layer = layers.ReshapeLayer(dense_layer2.output, (input_var.shape[0], 350, 1, 1))
+
+    # Tranposed conv layer : output.shape = (batch_size, 60, 3, 3)
+    transconv_layer1 = TransposedConvLayer(reshape_layer, num_filters=nfilters[0], filter_size=filter_size[0])
+
+    # Tranposed conv layer : output.shape = (batch_size, 30, 7, 7)
+    transconv_layer2 = TransposedConvLayer(transconv_layer1.output, num_filters=nfilters[1], filter_size=filter_size[1])
+
+    # Unpool layer : output.shape = (batch_size, 30, 14, 14)
+    unpool_layer1 = UnpoolLayer(transconv_layer2.output)
+
+    # Tranposed conv layer : output.shape = (batch_size, 10, 15, 15)
+    transconv_layer3 = TransposedConvLayer(unpool_layer1.output, num_filters=nfilters[2], filter_size=filter_size[2],
+                                           stride=(1, 1))
+
+    # Tranposed conv layer : output.shape = (batch_size, 3, 32, 32)
+    transconv_layer4 = TransposedConvLayer(transconv_layer3.output, num_filters=nfilters[3], filter_size=filter_size[3])
+
+    return transconv_layer4.output
+
+
