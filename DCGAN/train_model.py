b417bbf7baf5fe86ece0de9971cdf32d3de274ff
julianzaidi <julian.zaidi1@gmail.com>
2017-04-25 15:46:19 -0400

    DCGAN Network

--- DCGAN/train_model.py
+++ DCGAN/train_model.py
@@ -104,8 +104,8 @@ def train_model(learning_rate_dis=0.0002, learning_rate_gen=0.0002, n_epochs=2, 
     print('... Training')
 
     epoch = 0
-    nb_train_dis = 15
-    nb_train_gen = 5
+    nb_train_dis = 10
+    nb_train_gen = 1
     nb_batch = 10000 // batch_size
     nb_block = nb_batch // nb_train_dis
     # nb_block = nb_batch // nb_train_gen
